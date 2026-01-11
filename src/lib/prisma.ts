f5e33e71c06bed959f1e04a883c263abde9214fe
codeotherapist <codeo.therapist995@gmail.com>
2026-01-11 12:27:01 +0530

    prisma fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prismaClientSingleton = () => {
-    return new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasourceUrl:
+      process.env.NODE_ENV === "production"
+        ? process.env.DATABASE_URL_POOLED
+        : process.env.DATABASE_URL,
+  });
 
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
-declare const globalThis: {
-    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
